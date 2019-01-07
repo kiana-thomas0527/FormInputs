1b3f3ee80ee56faf2a5220a826b6959fe6e724a2
Kiana <kianataylor0527@gmail.com>
2019-01-07 23:35:42 +0000

    added pseudo code

--- flask_app.py
+++ flask_app.py
@@ -9,8 +9,15 @@ def main():
 @app.route('/process_inputs', methods=['POST'])
 def process_inputs():
     name = request.form.get('input_name', '')
+    if name == "":
+        return render_template
     dropdown = request.form.get('input_dropdown', '')
     select = request.form.get('input_select', '')
     freeform = request.form.get('input_freeform', '')
     return render_template("main_page.html", input_data=dropdown,
                            output="You're a wizard %s." % name)
+
+
+# this is going to help someone identify what their marvel superhero is
+# What is your favorite color?
+# What is your favorite superpower?
